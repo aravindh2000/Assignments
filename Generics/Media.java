df97ee34406e354002e124df26582d13a697b7bf
aravindh2000 <78861911+aravindh2000@users.noreply.github.com>
2022-11-20 14:30:09 +0530

    edit-branch-commit

--- Generics/Media.java
+++ Generics/Media.java
@@ -3,65 +3,67 @@ package employeedatabase;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Media <T>{
+public class Media<T> {
     private T mediaType;
-    List<T> lst=new ArrayList<>();
+    List<T> lst = new ArrayList<>();
 
-    public  Media(T mediaType)
-    {
-        this.mediaType=mediaType;
+    public Media(T mediaType) {
+        this.mediaType = mediaType;
         lst.add(mediaType);
     }
 
+    void showData() {
+        for (T t : lst) {
+            Book bk = null;
+            Video vd = null;
+            if (t instanceof Book) {
+                bk = (Book) t;
+                System.out.println(bk.getName());
 
-
-    void showData()
-    {
-       for (T a:lst)
-       {
-           Book bk=null;
-           Video vd=null;
-           if(t instanceof Book)
-           {
-               bk=(Book)t;
-               System.out.println(bk.getName());
-
-           }
-       }
+            }
+        }
     }
 
     public static void main(String[] args) {
-        Media<Book> media1=new Media<>(new Book("Harry"));
+        Media<Book> media1 = new Media<>(new Book("Harry"));
         media1.showData();
     }
 
 }
-class Book
-{
-    String name;
-    Book(String name){this.name=name;}
 
-    public String getName() {
-        return name;
+class Book {
+    String name;
+
+    Book(String name) {
+        this.name = name;
     }
-}
-class Video
-{
-    String name;
-    Video(String name){this.name=name;}
-
-    public String getName() {
-        return name;
-    }
-}
-class NewsPaper
-{
-
-    String name;
-    NewsPaper(String name){this.name=name;}
 
     public String getName() {
         return name;
     }
 }
 
+class Video {
+    String name;
+
+    Video(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class NewsPaper {
+
+    String name;
+
+    NewsPaper(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
